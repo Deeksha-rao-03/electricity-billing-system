89b4778c134b3a253ef970ac4b5442e9229386f0
Deeksha-rao-03 <deeksha.nv03@gmail.com>
2021-10-10 01:45:33 +0530

    first commit with home page

---
+++ src/Components/Landing/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FetchAllConsumption from '../Consumptions/FetchAllConsumption';
+import Footer from './Footer';
+import Header from './Header';
+
+function Home() {
+    return(
+        <div>
+            <Header/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home;
